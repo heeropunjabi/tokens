6f15f808feca884a5932ef35b53c7bb1cdf7eb93
Heero Punjabi <heeropunjabi@Heeros-MacBook-Pro.local>
2019-09-12 00:23:04 +0530

    Updates

---
+++ precache-manifest.57edb3f49adadb50b4b33db225f6ad87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "085db9fedd18141ad126",
+    "url": "/hotel-tokens/static/js/runtime~main.085db9fe.js"
+  },
+  {
+    "revision": "bc13a80fc407f4c38007",
+    "url": "/hotel-tokens/static/js/main.bc13a80f.chunk.js"
+  },
+  {
+    "revision": "da945d1fc84480a68140",
+    "url": "/hotel-tokens/static/js/1.da945d1f.chunk.js"
+  },
+  {
+    "revision": "bc13a80fc407f4c38007",
+    "url": "/hotel-tokens/static/css/main.f4864c95.chunk.css"
+  },
+  {
+    "revision": "da945d1fc84480a68140",
+    "url": "/hotel-tokens/static/css/1.76f3066f.chunk.css"
+  },
+  {
+    "revision": "002032fc60107d9fa50bbdde6e7b64ef",
+    "url": "/hotel-tokens/index.html"
+  }
+];
